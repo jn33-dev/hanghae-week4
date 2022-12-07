db2720c0e85cbbde2efb327ff6a2a63a43496978
yena choi <geniusyena332@gmail.com>
2022-12-07 10:36:28 +0900

    config/config.js: test와 production db 설정 변경 / development db와 동일하게 변경

--- config/config.js
+++ config/config.js
@@ -10,18 +10,18 @@ const development = {
 };
 
 const test = {
-  username: "root",
-  password: null,
-  database: "database_test",
-  host: "127.0.0.1",
+  username: env.MYSQL_USER,
+  password: env.MYSQL_PW,
+  database: env.MYSQL_DB,
+  host: env.MYSQL_HOST,
   dialect: "mysql",
 };
 
 const production = {
-  username: "root",
-  password: null,
-  database: "database_production",
-  host: "127.0.0.1",
+  username: env.MYSQL_USER,
+  password: env.MYSQL_PW,
+  database: env.MYSQL_DB,
+  host: env.MYSQL_HOST,
   dialect: "mysql",
 };
 
